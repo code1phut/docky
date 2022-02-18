c1203900c651658d693e2d4d3bdc15366c84af35
anhnq-tda <anh.nguyen1@tda.company>
2022-02-18 10:37:33 +0700

    init

---
+++ CoreServiceProvider.php
@@ -0,0 +1,21 @@
+namespace Ducky\Core;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider {
+    /**
+     * Bootstrap the application services.
+     * @return void
+     */
+     public function boot() {
+
+     }
+
+     /**
+      * Register the application services
+      * @return void
+      */
+    public function register() {
+
+    }
+}
